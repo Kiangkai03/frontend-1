6406a6640d10c113d4d345f2d6b1ea0b31d40dc1
Kiangkai03 <mistergood0071@gmail.com>
2021-11-05 12:28:43 +0700

    12

---
+++ components/layouts/admin.hoc.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import AdminHeader from './admin.header'
+import AdminSidebar from './admin.sidebar';
+import AdminFooter from './admin.footer';
+
+export default class AdminHOC extends Component {
+  render(){
+  return (
+      <div className="wrapper">
+        <AdminHeader />
+        <AdminSidebar />
+        {this.props.children}
+        <AdminFooter />
+      </div>
+  )
+  }
+}
